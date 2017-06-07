5549a3fcefeb303dc63ff96aff3d21d72ef41e3d
jyzHome <jyzgo@163.com>
2017-06-07 17:17:44 +0800

    update ads id

--- BallCollision/Assets/Scripts/Ad/AdMgr.cs
+++ BallCollision/Assets/Scripts/Ad/AdMgr.cs
@@ -7,33 +7,13 @@ using UnityEngine;
 
 public static class AdMgr  {
     #region Admob-------------------------
-//#if (UNITY_IOS)
-//    const string BANNER_ID = "ca-app-pub-8151883983314364/7268920130";
-    
-//    const string INTERSTITIAL_ID = "ca-app-pub-8151883983314364/4175852934";
-////#elif (UNITY_ANDROID)
 
-//    const string MAGIC_NATIVE_BANNER_ID = "ca-app-pub-8151883983314364/2313572938";
-//    const string MAGIC_INTERSTITIAL_ID = "ca-app-pub-8151883983314364/3790306139";
-//    const string MAGIC_BANNER = "ca-app-pub-8151883983314364/7016837337";
-    //#endif
+    const string BALLZ_ANDROID_INTER = "ca-app-pub-6770182166257156/2533736221";
+    const string BALLZ_ANDROID_BANNER = "ca-app-pub-6770182166257156/1057003028";
 
-
-    const string MAGIC_IOS_INTERSTITIAL_ID = "ca-app-pub-8151883983314364/4175852934";
-    const string MAGIC_IOS_BANNER_ID = "ca-app-pub-8151883983314364/7268920130";
-
-
-    const string OMG_IOS_INSTERSTITIAL_ID = "ca-app-pub-9169799985632280/8700464051";
-    const string OMG_IOS_BANNER_ID = "ca-app-pub-9169799985632280/4013654050";
-
-
-    //#if (UNITY_IOS)
-    //    const string OMG_NATIVE_BANNER_ID = "ca-app-pub-9169799985632280/7223730851";
-    //    const string OMG_INTERSTITIAL_ID = "ca-app-pub-9169799985632280/8700464051";
-    //#elif (UNITY_ANDROID)
-    //    const string OMG_NATIVE_BANNER_ID = "ca-app-pub-9169799985632280/9572081659";
-    //    const string OMG_INTERSTITIAL_ID = "ca-app-pub-9169799985632280/2048814858";
-    //#endif
+    const string BALLZ_IOS_INTER = "ca-app-pub-6770182166257156/9917402222";
+    const string BALLZ_IOS_BANNER = "ca-app-pub-6770182166257156/6963935823";
+ 
 
 
 
@@ -47,13 +27,9 @@ public static class AdMgr  {
 
 
 #if UNITY_ANDROID
-        string adUnitId =  //SettingMgr.current._adState.AndroidInterstitialID();
-                           // MAGIC_IOS_INTERSTITIAL_ID;
-       OMG_IOS_INSTERSTITIAL_ID;
+        string adUnitId = BALLZ_ANDROID_INTER;
 #elif UNITY_IOS
-        string adUnitId = //SettingMgr.current._adState.IOSInterstitialID(); 
-        //MAGIC_IOS_INTERSTITIAL_ID;
-        OMG_IOS_INSTERSTITIAL_ID;
+        string adUnitId = BALLZ_IOS_INTER; 
 #else
         string adUnitId = "unexpected_platform";
 #endif
@@ -133,20 +109,17 @@ public static class AdMgr  {
 
 
 
-    const string ZL_AND_BANNER = "ca-app-pub-7903543313139585/9520800753";
-    const string ZL_IOS_BANNER = "";
+
 
     static void  InitBanner()
     {
         //#if UNITY_EDITOR
         //        string adUnitId = "unused";
 #if UNITY_ANDROID
-        string adUnitId =// MAGIC_IOS_BANNER_ID;//ZL_AND_BANNER;
-            ZL_AND_BANNER;
+        string adUnitId = BALLZ_ANDROID_BANNER;
 #elif UNITY_IOS
 
-        string adUnitId = //MAGIC_IOS_BANNER_ID;//ZL_AND_BANNER;
-          OMG_IOS_BANNER_ID;
+        string adUnitId = BALLZ_IOS_BANNER;
 #else
         string adUnitId = "unexpected_platform";
 #endif
